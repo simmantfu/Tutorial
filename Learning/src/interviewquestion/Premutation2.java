7a772b71daf59269265e5e791d4d3d053970b522
yadavsim <yadavsim@FCB-23127-D.INGDC.NET>
2018-01-10 19:22:25 +0000

    Remove unused methods

--- Learning/src/interviewquestion/Premutation2.java
+++ Learning/src/interviewquestion/Premutation2.java
@@ -7,17 +7,15 @@ import java.util.TreeSet;
 public class Premutation2 {
 
 	static void getPrem(String s) {
-		
+
 		int stringCount = fact(s.length());
 
 		System.out.println(stringCount);
-		
+
 		String[] a = s.split("");
-		String[] c = rev(s).split("");
-		
+
 		Set<String> set = printCom(a, stringCount);
-		set.forEach(k->System.out.println(k));
-		
+		set.forEach(k -> System.out.println(k));
 
 	}
 
@@ -26,11 +24,11 @@ public class Premutation2 {
 		Set<String> set = new TreeSet<>();
 		for (String a1 : a)
 			st.add(a1);
-		for (int i = 0; i < stringCount/2; i++) {
+		for (int i = 0; i < stringCount / 2; i++) {
 			set.add(st.toString());
 			st.addLast(st.pop());
 		}
-		
+
 		return set;
 	}
 
@@ -41,15 +39,6 @@ public class Premutation2 {
 		}
 
 		return (k * fact(k - 1));
-	}
-
-	static String rev(String a) {
-		String result = "";
-		char[] ar = a.toCharArray();
-		for (int j = ar.length - 1; j >= 0; j--) {
-			result = result + ar[j] + "";
-		}
-		return result;
 	}
 
 	public static void main(String[] args) {
